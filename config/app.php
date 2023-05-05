89e137d9dde6e14809db4abacb12db6102935016
Hasbullah Almughniy <51769538+hasbullah-mughniy@users.noreply.github.com>
2023-05-05 09:44:47 +0700

    add terminal

--- config/app.php
+++ config/app.php
@@ -199,6 +199,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Recca0120\Terminal\TerminalServiceProvider::class,
+
     ],
 
     /*
